b4aafef1813e27b5b40fdb4eeb13d82e9344b0af
xunxing <xunxing1989@163.com>
2021-05-14 00:37:33 +0800

    feat: add array.h.c

---
+++ array/Array.h.c
@@ -0,0 +1,48 @@
+//
+// Created by 毛毛 on 2021/5/14.
+//
+
+#ifndef __ARRAY_H__
+#define __ARRAY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typeof struct Array {
+    /**
+     * size
+     */
+    size_t size;
+    size_t len;
+    size_t typeSize;
+    void (*dup)(void *ptr, void *key);
+    void (*free)(void *ptr);
+    int (*match)(void *ptr, void *key);
+    void *p;
+} Array;
+
+#define arraySetDupMethod(a, m) ((a)->dup = (m))
+#define arraySetFreeMethod(a, m) ((a)->free = (m))
+#define arraySetMatchMethod(a, m) ((a)->match = (m))
+
+#define arrayGetDupMethod(a) ((a)->dup)
+#define arrayGetFree(a) ((a)->free)
+#define arrayGetMatchMethod(a) ((a)->match)
+
+Array* arrayCreate();//?
+void arrayInit(Array *array, int size, int typeSize);
+
+int arrayInsert(Array *array, size_t pos, void *const value);//? void *const
+size_t arraySearchValue(Array *array, void *const value);
+void* arrayIndex(Array *array, size_t index);//?
+int arrayModify(Array *array, size_t pos, void *const value);
+
+size_t arrayLen(Array *array);
+size_t arraySize(Array *array);
+
+void arrayEmpty(Array *array);
+void arrayDelValue(Array *array, void *value);
+void arrayDelIndex(Array *array, size_t pos);
+
+#endif // !__ARRAY_H__
+
--- array/array.c
+++ array/array.c
@@ -1,4 +1,6 @@
 //
+// dynamic array: from small to big
+//
 // Created by 毛毛 on 2021/5/13.
 //
 
@@ -6,17 +8,81 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * size is the array size
+ * used is the array size that had already used
+ * arr is the pointer to the array that save values
+ */
 struct array {
-    int size;
-    int used;
-    int *arr;
+    int size;//total size
+    int usedSize;// current length
+    int *arr;// save data
 };
 
+/**
+ * to print all the array values
+ * @param array  the array struct pointer
+ */
 void dump(struct array *array) {
-    for(int i = 0; i < array->used; i++) {
+    for(int i = 0; i < array->usedSize; i++) {
         printf("[%02d]: %08d\n", i, array->arr[i]);
     }
 }
+
+/**
+ * to allocate size to the pointer array by the struct array size
+ * @param array the array struct pointer
+ */
 void alloc(struct array *array) {
-    array->arr = (int *)
+    array->arr = (int *)malloc(array->size * sizeof(int))
+}
+
+/**
+ *
+ * @param array the struct array pointer
+ * @param elem the new inserted element
+ * @return
+ */
+int insert(struct array *array, int elem) {
+    int idx;
+    if(array->usedSize >= array->size) {
+        return -1;
+    }
+    // find the index
+    for(idx = 0; idx < array->usedSize; idx++) {
+        if(array->arr[idx] > elem) {
+            break;
+        }
+    }
+
+    // need to move the bigger array
+    if(idx < array->usedSize) {
+        // array->usedSize - idx right part length
+        memmove(&array->arr[idx+1], &array->arr[idx], (array->usedSize - idx) * sizeof (int))
+    }
+    array->arr[idx] = elem;
+    array->used++;
+    return idx;
+}
+
+int delete(struct  array *array, int idx) {
+    if(idx < 0 || idx >= array->usedSize) {
+        return -1;
+    }
+    memmove(&array->arr[idx], &array->arr[idx+1], (array->usedSize - idx - 1)*sizeof (int));
+    array->usedSize--;
+    return 0;
+}
+
+int search(struct array *array, int elem) {
+    int idx;
+    for(idx = 0; idx < array->usedSize; idx++) {
+        if(array->arr[idx] == elem) {
+            return idx;
+        }
+        if(array->arr[idx] > elem) {
+            return -1;
+        }
+    }
+    return -1;
 }
